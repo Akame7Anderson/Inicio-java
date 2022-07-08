b4273432712a2263d3da8294a6e02de6da70c1d4
Anderson (Akame!) <andersonrobertopetry@gmail.com>
2022-07-08 10:19:35 -0300

    var's

--- src/Programa.java
+++ src/Programa.java
@@ -2,5 +2,14 @@
 public class Programa {
 	public static void main(String[] args) {
 		System.out.println("Ola Mundo");
+		
+		int idade;
+		idade = 37;
+		System.out.println(idade);
+		
+		
+		int operacao;
+		operacao = 3 * 5 - 1;
+		System.out.println("O resultado da operação acima e " + operacao);
 	}
 }
